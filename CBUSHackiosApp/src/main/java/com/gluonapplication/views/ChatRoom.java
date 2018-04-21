be0f5986eb2c6f42c84dfc607219917d87794331
Jkim-Hack <johnkim1108@gmail.com>
2018-04-21 16:15:37 -0400

    Added a shat load of stuff

---
+++ CBUSHackiosApp/src/main/java/com/gluonapplication/views/ChatRoom.java
@@ -0,0 +1,48 @@
+package com.gluonapplication.views;
+
+import com.gluonhq.charm.glisten.mvc.View;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ChatRoom extends View {
+
+
+
+
+
+    public ChatRoom(String name) {
+        super(name);
+
+
+
+        DatabaseReference ref = FirebaseDatabase.getInstance()
+                .getReference("Chatroom1");
+
+        DatabaseReference userRef = ref;
+
+
+        TextField input = new TextField();
+        input.setPromptText("Message");
+
+        Button send = new Button("Send");
+
+        send.setOnAction((ActionEvent e) -> {
+
+            ref.setValueAsync(input.getText());
+
+        });
+        ListVi
+
+
+
+
+    }
+
+
+
+
+
+}
